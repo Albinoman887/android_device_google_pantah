4b166f3f775e44b85d35ea2e789e60badaa154f8
Matt Filetto <matt.filetto@gmail.com>
2022-09-27 01:37:44 -0700

    StatixOS -> SparkOS
    * changed all statix references to spark
    * create device-spark.mk for rom specific params
    * added artifacts allow list
    * drop statix.dependencies
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

---
+++ spark_panther.mk
@@ -0,0 +1,28 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common SparkOS stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/pantah/aosp_panther.mk)
+$(call inherit-product, device/google/pantah/device-spark.mk)
+
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.gs101
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := spark_panther
+PRODUCT_MODEL := Pixel 7
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=panther \
+    PRIVATE_BUILD_DESC="panther-user 13 TD1A.220804.031 9071314 release-keys"
+
+BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
