79f6f125e437adf4ae987beb5cd3ecb03f6b86bb
Matt Filetto <matt.filetto@gmail.com>
2023-01-06 17:18:33 -0800

    disable pe faceunlock
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -22,9 +22,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
-# Pixel Face Enrollment
-PRODUCT_PACKAGES += \
-    PixelFaceEnroll
+TARGET_FACE_UNLOCK_SUPPORTED := false
 
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
