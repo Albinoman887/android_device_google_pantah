3eed51ee5234981027171d0459c1920e78486868
Matt Filetto <matt.filetto@gmail.com>
2023-02-01 08:05:40 -0800

    pantah: SELinux: Ignore neverallows so kirisakura Kernel works correctly
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-spark.mk
+++ device-spark.mk
@@ -16,6 +16,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.service.debuggable=1 \
     persist.service.adb.enable=1
 
+# SELinux
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # properties - product
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
