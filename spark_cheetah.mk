b0f2b63d2c245ec35a6a205ade6cca8b29d26e37
Matt Filetto <matt.filetto@gmail.com>
2023-02-14 02:36:34 -0800

    pantah: inherit pixelparts
    * Powershare HAL and pixel Touch hal amung others are included there
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- spark_cheetah.mk
+++ spark_cheetah.mk
@@ -14,8 +14,11 @@ TARGET_SUPPORTS_QUICK_TAP := true
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
 $(call inherit-product, device/google/gs201/spark_common.mk)
-
 include device/google/pantah/cheetah/device-spark.mk
+
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
+$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
