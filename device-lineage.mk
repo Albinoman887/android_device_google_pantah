5b890ca4086a78936a26cb941aef5930a00be16a
Matt Filetto <matt.filetto@gmail.com>
2023-04-14 02:36:43 -0700

    pantah: build with GMS by default
    Change-Id: I264fa25f5413cbff55ef3c19323e79baed2d4e35
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,6 +11,12 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+
+# GMS
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
 
