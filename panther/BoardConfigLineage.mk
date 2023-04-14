95c504e1ca1f8165940cbe652f7002e369081d0a
Matt Filetto <matt.filetto@gmail.com>
2023-04-14 03:38:24 -0700

    panther: override device name to Pixel 7
    Change-Id: I906d7116b3d8ad7135238e54fd5a45ddb1bb21d3
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- panther/BoardConfigLineage.mk
+++ panther/BoardConfigLineage.mk
@@ -3,6 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+DEVICE_PATH := device/google/pantah/panther
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_panther
 
 include device/google/pantah/BoardConfigLineage.mk
 
