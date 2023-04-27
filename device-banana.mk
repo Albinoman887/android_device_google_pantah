f701f89792687a861013d9976bb3c661d9dd9b03
Matt Filetto <matt.filetto@gmail.com>
2023-04-27 11:09:36 -0700

    pantah: build all GMS Packages
    Change-Id: Id4a360f877cc5893b76901ef6cc5d975898796cd
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    # Conflicts:
    #	device-banana.mk
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-banana.mk
+++ device-banana.mk
@@ -15,6 +15,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.aux.camera_oem_package=com.google.android.GoogleCamera
 
+# GMS
+WITH_GMS := true
+TARGET_CORE_GMS := false
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
 # sysconfig and permissions XML from stock
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml \
