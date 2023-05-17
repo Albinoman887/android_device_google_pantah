7566013b23e536653c63987b6f62065d4f8f209d
Matt Filetto <matt.filetto@gmail.com>
2023-05-17 01:20:49 -0700

    pantah: Alpha-Droid bringup
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_panther.mk
+++ lineage_panther.mk
@@ -6,19 +6,21 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/banana/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_panther.mk)
-$(call inherit-product, device/google/gs201/banana_common.mk)
+$(call inherit-product, device/google/gs201/lineage_common.mk)
 
-include device/google/pantah/panther/device-banana.mk
+include device/google/pantah/panther/device-lineage.mk
 
-$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
+# Parts
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := banana_panther
+PRODUCT_NAME := lineage_panther
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -26,7 +28,6 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=panther \
-    PRIVATE_BUILD_DESC="panther-user 13 TQ3A.230705.001 10216780 release-keys"
+    PRIVATE_BUILD_DESC="panther-user 13 TQ3A.230605.012 10204971 release-keys"
 
-BUILD_FINGERPRINT := google/panther/panther:13/TQ3A.230705.001/10216780:user/release-keys
-
+BUILD_FINGERPRINT := google/panther/panther:13/TQ3A.230605.012/10204971:user/release-keys
