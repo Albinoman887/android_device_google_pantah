d197aeb61c72a38e4fa87ef26bff3fc47c1faec9
Matt Filetto <matt.filetto@gmail.com>
2023-06-20 00:54:44 -0700

    pantah: Add new RRO overlays to ensure all pixel features are enabled.
    * Various pixel features require changes across the ROM source to some resource overlays to enable features like Ambient Music detection. Instead of going around to different repos and changing single lines of code use an RRO overlay to explicitly set all the required overlays at runtime.
    
    * (note: the inclusion of force enabling com.google.android.as/com.google.intelligence.sense.ambientmusic* in both the PixelFeaturesOverlay and LineageSdkNowPlayingOverlay is NOT a mistake. Lineage-sdk by default always disables these. This ensures both locations are overwritten to enable the feature). Trust me this will save you a lot of time and headaches tracking down why the feature wasn't workiing.
    
    (Credits: @StatiXOS for the idea)
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -7,6 +7,11 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+# RRO Overlays
+PRODUCT_PACKAGES +=
+    PixelFeaturesOverlay \
+    NowPlayingOverlay
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
