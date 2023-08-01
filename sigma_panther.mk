acd8fa191391edee8ecadea9e79525ef0e772947
Matt Filetto <matt.filetto@gmail.com>
2023-07-31 17:37:46 -0700

    panther: sigmafy your life
    * lineage_panther -> sigma_panther
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- sigma_panther.mk
+++ sigma_panther.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Sigma stuff.
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -14,13 +14,10 @@ $(call inherit-product, device/google/gs201/lineage_common.mk)
 
 include device/google/pantah/panther/device-lineage.mk
 
-# Parts
-$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
-
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7
-PRODUCT_NAME := lineage_panther
+PRODUCT_NAME := sigma_panther
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
